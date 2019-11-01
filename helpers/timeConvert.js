2ee17cf0f7af1ca8ef84cbd24772a38bbde9066c
Alex Rothenberg <alexroth96@gmail.com>
2019-10-31 20:37:35 -0400

    fix create profile design

--- helpers/timeConvert.js
+++ helpers/timeConvert.js
@@ -1,7 +1,17 @@
 exports.timeConvert = time => {
+	//fix when time is "12:00" to not be 0
+	//dry up this algo
 	const splitTime = time.split(':');
-	const toTwelveHour = 12 - splitTime[0];
-	return splitTime[0] > 12 ? `${[-toTwelveHour, ':', splitTime[1]].join('')}PM` : `${time}AM`;
+	const toNums = splitTime.map(num => parseInt(num));
+	const toTwelveHour = -(12 - toNums[0]);
+	if (toNums[0] > 12) {
+		return `${[toTwelveHour, ':', splitTime[1]].join('')}PM`;
+	} else if (toNums[0] === 12) {
+		return `${time}PM`;
+	} else if (toNums[0] < 1) {
+		return `${[12, ':', splitTime[1]].join('')}AM`;
+	}
+	return `${time}AM`;
 };
 
 module.exports = exports;
