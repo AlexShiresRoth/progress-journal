8614578a4bae79294ee9dd8c34352fe466673623
Alex Rothenberg <alexroth96@gmail.com>
2020-01-27 00:01:02 -0500

    redesign landing page and forms

--- app.js
+++ app.js
@@ -1,46 +1,46 @@
-//require('dotenv').config();
-const createError = require("http-errors");
-const express = require("express");
-const path = require("path");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const passport = require("passport");
-const flash = require("connect-flash");
-const LocalStrategy = require("passport-local");
-const User = require("./models/user");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
-const cors = require("cors");
+require('dotenv').config();
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const flash = require('connect-flash');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
 
-const indexRouter = require("./routes/index");
-const daysRouter = require("./routes/days");
-const profileRouter = require("./routes/profile");
-const searchRouter = require("./routes/search");
-const todosRouter = require("./routes/todos");
-const goalsRouter = require("./routes/goals");
+const indexRouter = require('./routes/index');
+const daysRouter = require('./routes/days');
+const profileRouter = require('./routes/profile');
+const searchRouter = require('./routes/search');
+const todosRouter = require('./routes/todos');
+const goalsRouter = require('./routes/goals');
 
 const app = express();
 app.use(cors());
 
 // view engine setup
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
 //mongoose setup
 const mongodburi = process.env.MONGO_URI;
-mongoose.set("debug", true);
+mongoose.set('debug', true);
 mongoose.connect(mongodburi, {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useCreateIndex: true
+	useNewUrlParser: true,
+	useFindAndModify: false,
+	useCreateIndex: true,
 });
 
 //express-session setup
 app.use(
-  require("express-session")({
-    secret: "Snacky",
-    resave: false,
-    saveUninitialized: false
-  })
+	require('express-session')({
+		secret: 'Snacky',
+		resave: false,
+		saveUninitialized: false,
+	})
 );
 
 //passport setup//
@@ -53,44 +53,44 @@ passport.deserializeUser(User.deserializeUser());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "/client")));
-app.use(express.static(path.join(__dirname, "/public")));
-app.use(express.static(path.join(__dirname, "/views")));
+app.use(express.static(path.join(__dirname, '/client')));
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, '/views')));
 app.use(flash());
 
 app.use((req, res, next) => {
-  res.locals.currentUser = req.user;
-  res.locals.error = req.flash("error");
-  res.locals.success = req.flash("success");
-  next();
+	res.locals.currentUser = req.user;
+	res.locals.error = req.flash('error');
+	res.locals.success = req.flash('success');
+	next();
 });
 
 //routes
-app.use("/", indexRouter);
-app.use("/profile", profileRouter);
-app.use("/api/days", daysRouter);
-app.use("/api/search", searchRouter);
-app.use("/api/todos", todosRouter);
-app.use("/api/goals", goalsRouter);
+app.use('/', indexRouter);
+app.use('/profile', profileRouter);
+app.use('/api/days', daysRouter);
+app.use('/api/search', searchRouter);
+app.use('/api/todos', todosRouter);
+app.use('/api/goals', goalsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+	next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
 });
 
 module.exports = app;
